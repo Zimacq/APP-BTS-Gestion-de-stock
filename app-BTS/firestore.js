a58097763dfce7f7d3f9c06e6b853b3b0ca0b475
Zimacq <147037243+Zimacq@users.noreply.github.com>
2024-04-29 09:50:52 +0200

    Delete app-BTS directory

--- app-BTS/firestore.js
+++
@@ -1,8 +0,0 @@
-import {app} from "./firebase"
-import { getFirestore, collection, getDocs  } from "firebase/firestore";
-
-const db = getFirestore(app);
-
-const EleveCollection = async () => {await getDocs(collection(db, "Eleve"))};
-
-export {EleveCollection}
